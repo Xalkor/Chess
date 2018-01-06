41b2e6547a7581252034e8f4ca7a0b1cdf9a3195
10000turtles <10000turtles@gmail.com>
2018-01-06 17:39:53 -0500

    Update Knight.java

--- CHESS/src/chess/Knight.java
+++ CHESS/src/chess/Knight.java
@@ -12,6 +12,35 @@ public class Knight extends ChessPiece{
          this.position = position;
          board.board[this.position.x][this.position.y] = this;   
     }
-    Pos[] possibleMoves(){return new Pos[0];}
+    Pos[] possibleMoves(ChessBoard board){
+        Pos[] valid = new Pos[8];
+        int nullCounter = 0;
+        valid[0] = new Pos(position.x+2,position.y-1);
+        valid[1] = new Pos(position.x+2,position.y+1);
+        valid[2] = new Pos(position.x+1,position.y-2);
+        valid[3] = new Pos(position.x+1,position.y+2);
+        valid[4] = new Pos(position.x-2,position.y-1);
+        valid[5] = new Pos(position.x-2,position.y+1);
+        valid[6] = new Pos(position.x-1,position.y-2);
+        valid[7] = new Pos(position.x-1,position.y+2);
+        for(int i = 0;i<8;i++) {
+            if (valid[i].x > 7 || valid[i].x < 0 || valid[i].y > 7 || valid[i].y < 0||(board.board[valid[i].x][valid[i].y]!=null&&(board.board[valid[i].x][valid[i].y].isWhite==isWhite))){
+                valid[i] = null;
+                nullCounter++;
+            }
+        }
+        Pos[] possibleMoves = new Pos[8-nullCounter];
+        for(int i = 0,j = 0;i<possibleMoves.length;i++, j++) {
+            while(valid[j]==null)j++;
+            possibleMoves[i] = valid[j];
+            if(board.board[valid[i].x][valid[i].y]!=null&&(board.board[valid[i].x][valid[i].y].isWhite!=isWhite)) {
+
+            }
+
+        }
+        return new Pos[0];
+    }
+
+    }
     public String toString(){return isWhite ? "[h]" : "[H]";}
 }
